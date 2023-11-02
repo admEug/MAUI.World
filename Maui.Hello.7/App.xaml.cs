f0a67c27a1f0aa223cb2ba3db476c56294579a6b
admEug <zmaeul@gmail.com>
2023-11-02 12:34:02 +0200

    Create a MAUI.7 Application and setup the MainPage as MainPage()

---
+++ Maui.Hello.7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui.Hello._7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Maui.Hello.7/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui.Hello._7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
