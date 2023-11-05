81fa41801ce20bde7c98f5cad3420061e620b4f5
admEug <zmaeul@gmail.com>
2023-11-05 18:11:43 +0200

    DataBindings, Working external resources

---
+++ Apps/DatabindingDemo/DatabindingDemo/Models/Person.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DatabindingDemo.Models
+{
+    public class Person : INotifyPropertyChanged
+     {
+        private string name;
+        private string phone;
+        private string address;
+
+        public string Name
+          {
+               get => name; 
+               set
+               {
+                    name = value;
+                    OnPropertyChanged();
+               }
+          }
+          public string Phone
+          {
+               get => phone; set
+               {
+                    phone = value;
+                    OnPropertyChanged();
+               }
+          }
+          public string Address
+          {
+               get => address; set
+               {
+                    address = value;
+                    OnPropertyChanged();
+               }
+          }
+
+          public event PropertyChangedEventHandler PropertyChanged;
+
+          protected void OnPropertyChanged([CallerMemberName] string propName = null)
+          {
+               PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(propName));
+          }
+     }
+}
